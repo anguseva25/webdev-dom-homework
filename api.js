ed4be7e1577322c96cbe948d1c20c5caef7bb964
anguseva25 <annaguseva97@gmail.com>
2024-04-07 20:51:27 +0300

    corrected main.js (new render), api.js(authorization),

--- api.js
+++ api.js
@@ -1,5 +1,9 @@
+export let name = ''
+export let token = ''
+const host = 'https://wedev-api.sky.pro/api/user/login'
+
 export function getComments() {
-    return fetch("https://wedev-api.sky.pro/api/v1/anka-anny/comments", {
+    return fetch("https://wedev-api.sky.pro/api/v2/anka-anny/comments", {
         method: "GET",
     })
         .then((response) => {
@@ -11,13 +15,46 @@ export function getComments() {
         });
 }
 
-export function postComment( { text, nameText }) {
-    return fetch("https://wedev-api.sky.pro/api/v1/anka-anny/comments", {
+export function getAuthorization(login, password) {
+    return fetch(host, {
         method: "POST",
         body: JSON.stringify({
+            login: login,
+            password: password,
+        }),
+    })
+        .then((response) => {
+            if (response.status === 400)
+                // throw new Error("слишком короткий запрос. Укажи больше 3 символов");
+                return response.json();
+            if (response.status === 500)
+                // throw new Error("ошибка на сервере");
+                return response.json();
+            if (response.status === 201)
+                return response.json();
+        })
+        .then((data) => {
+            if (data.error)
+                return alert(data.error)
+
+            name = data.user.name
+            token = `Bearer ${data.user.token}`
+            return "Ok"
+        })
+}
+
+
+
+export function postComment( { text, nameText }) {
+    return fetch("https://wedev-api.sky.pro/api/v2/anka-anny/comments", {
+        method: "POST",
+        headers: {
+            Authorization: token,
+        },
+        body: JSON.stringify({
           text: text.trim().safeCode(),
-          name: nameText.trim().safeCode(),
-          forceError: true,
+          //name: nameText.trim().safeCode(),
+          //forceError: true,
         }),
       })
         .then((response) => {
--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 "use strict";
-import {getComments, postComment} from "./api.js"
+import {name, token, getAuthorization, getComments, postComment} from "./api.js"
 import {formatDate} from "./common.js"
 
-const buttonElement = document.getElementById("add-button");
-const listElement = document.getElementById("list-comments");
-const nameInputElement = document.getElementById("name-input");
-const textInputElement = document.getElementById("text-input");
-const commentsElement = document.getElementById("comments");
+const mainDiv = document.querySelector(".main")
 
-// массив данных c каждым пользователем
+let buttonElement
+let nameInputElement
+let textInputElement
+let listElement
+
+// загрузочная Ракета-анимация (основная)
+const preloader = document.getElementById("preloader")
+// загрузочная Ракета-анимация (дополнительная)
+let secondPreloader
+
+// массив данных с каждым пользователем
 let arrayUsers = [];
 
 const likeButton = () => {
@@ -25,8 +31,8 @@ const likeButton = () => {
                 user.validButtonLike = true;
             }
 
-            renderUsers();
-            event.stopPropagation();
+            render()
+            event.stopPropagation()
         });
 };
 
@@ -42,49 +48,96 @@ const commentAdd = () => {
             )}\n\n${user.name}, `;
             event.stopPropagation();
         });
-};
+}
+
+const render = (forComments = true) => {
+    let linesHTML
+
+    if (forComments) {
+        const comments = getLinesUsers()
+        linesHTML = [`<ul id="list-comments" class="comments">`, ...comments, `</ul>`]
+
+        if (token) {
+            linesHTML.push(`<div class="preloaderNew hide" id="preloaderNew">
+                  <span class="loaderNew"></span>
+                </div>`)
+            linesHTML.push(`<div class="add-form">
+                    <input type="text" id="name-input" class="add-form-name" placeholder="Введите ваше имя" value="${name}" disabled>
+                    <textarea id="text-input" class="add-form-text" placeholder="Введите ваш коментарий" rows="4"></textarea>
+                    <div class="add-form-row">
+                        <button id="add-button" class="add-form-button">Написать</button>
+                    </div>
+                </div>`)
+        } else {
+            linesHTML.push(`<p class="ask-login">Чтобы отправлять сообщения, перейдите на <a href="#" id="authorization">авторизацию</a></p>`)
+        }
+    } else {
+        linesHTML = [`<div class="sign-form">
+            <input type="text" id="login-input" class="sign-form-login" placeholder="Введите ваш логин" value="">
+            <input type="password" id="password-input" class="sign-form-password" placeholder="Введите ваш пароль" value="">
+            <div class="sign-form-row">
+                <button id="sign-button" class="sign-form-button">Авторизоваться</button>
+            </div>
+        </div>`]
+        linesHTML.push(`<p class="ask-login">Закрыть форму авторизации, <a href="#" id="authorization">перейти к комментариям</a></p>`)
+    }
+
+    mainDiv.innerHTML = linesHTML.join("");
+
+    const linkAuthorization = document.getElementById('authorization')
+
+    if (linkAuthorization)
+        linkAuthorization.addEventListener("click", () => render(!forComments))
+
+    secondPreloader = document.querySelector('.preloaderNew')
+    listElement = document.getElementById("list-comments")
+
+    if (token) {
+        buttonElement = document.getElementById("add-button")
+
+        buttonSendClick()
+        likeButton()
+    }
+    else {
+        buttonElement = document.getElementById("sign-button")
+
+        buttonAuthorizeClick()
+    }
+
+    //
+    // if (buttonElement.disabled) {
+    //     buttonElement.disabled = false;
+    //     buttonElement.textContent = "Написать";
+    // }
+}
 
 // render- функция (форма нового комментария)
-const renderUsers = () => {
-    listElement.innerHTML = arrayUsers
+function getLinesUsers(){
+    return arrayUsers
         // const userHtml = arrayUsers
         .map((user, index) => {
-            return `<li class="comment" data-index= '${index}'>
-        <div class="comment-header">
-          <div>${user.name}</div>
-          <div>${user.dateOfComment}</div>
-        </div>
-        <div class="comment-body">
-          <div class="comment-text" data-index= '${index}'>
-            ${user.text}
-          </div>
-        </div>
-        <div class="comment-footer">
-          <div class="likes">
-            <span class="likes-counter">${user.likes}</span>
-            <button data-index= '${index}' class="like-button ${
-                user.validButtonLike ? "-active-like" : ""
-            }"></button>
-          </div>
-        </div>
-      </li>`;
-        })
-        .join("");
-    // listElement.innerHTML = userHtml;
-    likeButton();
-    commentAdd();
-    if (buttonElement.disabled) {
-        buttonElement.disabled = false;
-        buttonElement.textContent = "Написать";
-    }
-};
+            const likeStatus = user.validButtonLike ? "-active-like" : ""
+            const dataIndex = `data-index="${index}"`
 
-renderUsers();
-
-// загрузочная Ракета-анимация (основная)
-const preloader = document.getElementById("preloader");
-// загрузочная Ракета-анимация (дополнительная)
-const secondPreloader = document.querySelector('.preloaderNew');
+            return `<li class="comment" ${dataIndex}>
+                <div class="comment-header">
+                    <div>${user.name}</div>
+                    <div>${user.dateOfComment}</div>
+                </div>
+                <div class="comment-body">
+                    <div class="comment-text" ${dataIndex}>
+                        ${user.text}
+                    </div>
+                </div>
+                <div class="comment-footer">
+                    <div class="likes">
+                        <span class="likes-counter">${user.likes}</span>
+                        <button class="like-button ${likeStatus}" ${dataIndex}></button>
+                    </div>
+                </div>
+              </li>`
+    })
+}
 
 window.addEventListener("load", function () {
     preloader.classList.remove('hide');
@@ -97,7 +150,7 @@ window.addEventListener("load", function () {
 
 // METHOD " GET "
 
-function fetchPromiceArr() {
+function fetchPromiseArr() {
     getComments().then((responseData) => {
         arrayUsers = responseData.comments.map((comment) => {
             return {
@@ -110,8 +163,9 @@ function fetchPromiceArr() {
             };
         });
         preloader.classList.add('hide');
-        secondPreloader.classList.add('hide');
-        renderUsers();
+        if (secondPreloader)
+            secondPreloader.classList.add('hide');
+        render()
     })
         .catch((error) => {
             if (error.message === "Failed to fetch")
@@ -123,7 +177,7 @@ function fetchPromiceArr() {
         });
 }
 
-fetchPromiceArr();
+fetchPromiseArr();
 
 // METHOD " POST "
 function postPromise() {
@@ -131,9 +185,8 @@ function postPromise() {
         text: textInputElement.value,
         nameText: nameInputElement.value
     }).then(() => {
-        nameInputElement.value = "";
         textInputElement.value = "";
-        fetchPromiceArr();
+        fetchPromiseArr();
     })
         .catch((error) => {
             if (error.message === "Failed to fetch")
@@ -147,31 +200,95 @@ function postPromise() {
             preloader.classList.add("hide");
 
             // return back previous comments
-            renderUsers()
+            render()
         });
 }
 
+// Нажатие кнопки авторизации
+
+function buttonAuthorizeClick() {
+    if (!buttonElement)
+        return
+
+    const loginInputElement = document.getElementById("login-input")
+    const passwordInputElement = document.getElementById("password-input")
+
+    if (!loginInputElement || !passwordInputElement)
+        return
+
+    buttonElement.addEventListener("click", () => {
+        // в случае пустого поля в графе login
+        loginInputElement.classList.remove("error");
+
+        if (loginInputElement.value.trim() === "") {
+            loginInputElement.classList.add("error");
+            return;
+        }
+
+        // в случае пустого поля в графе password
+        passwordInputElement.classList.remove("error");
+
+        if (passwordInputElement.value.trim() === "") {
+            passwordInputElement.classList.add("error");
+            return;
+        }
+
+        buttonElement.disabled = true
+        buttonElement.textContent = "Подождите..."
+
+        preloader.classList.remove("hide")
+
+        getAuthorization(loginInputElement.value, passwordInputElement.value)
+            .then((result) => {
+                preloader.classList.add("hide")
+
+                if (result === "Ok") {
+                    render()
+                } else {
+                    passwordInputElement.value = ""
+
+                    buttonElement.disabled = false
+                    buttonElement.textContent = "Авторизоваться"
+                }
+            })
+    })
+}
+
 // Работа кнопки при нажатии (поле Имя и поле текст)
-buttonElement.addEventListener("click", () => {
-    // в случае пустого поля в графе Имя
-    nameInputElement.classList.remove("error");
-    if (nameInputElement.value.trim() === "") {
-        nameInputElement.classList.add("error");
-        return;
-    }
-    // в случае пустого поля в графе Текст
-    textInputElement.classList.remove("error");
-    if (textInputElement.value.trim() === "") {
-        textInputElement.classList.add("error");
-        return;
-    }
 
-    listElement.innerHTML = ""
+function buttonSendClick() {
+    if (!buttonElement)
+        return
 
-    buttonElement.disabled = true;
-    buttonElement.textContent = "Элемент добавляется...";
-    preloader.classList.remove("hide");
+    nameInputElement = document.getElementById("name-input")
+    textInputElement = document.getElementById("text-input")
 
-    postPromise();
+    if (!nameInputElement || !textInputElement)
+        return
 
-});
+    buttonElement.addEventListener("click", () => {
+        // в случае пустого поля в графе Имя
+        nameInputElement.classList.remove("error");
+
+        if (nameInputElement.value.trim() === "") {
+            nameInputElement.classList.add("error");
+            return;
+        }
+
+        // в случае пустого поля в графе Текст
+        textInputElement.classList.remove("error");
+
+        if (textInputElement.value.trim() === "") {
+            textInputElement.classList.add("error");
+            return;
+        }
+
+        listElement.innerHTML = ""
+
+        buttonElement.disabled = true;
+        buttonElement.textContent = "Элемент добавляется...";
+        preloader.classList.remove("hide");
+
+        postPromise();
+    })
+}
